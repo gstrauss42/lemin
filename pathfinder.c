c6f99fd9f922c00d1a49c03eb536f57be9c756f6
Gabriel Strauss <gstrauss@c2r4s2.wethinkcode.co.za>
2020-01-10 12:02:11 +0200

    worked on pathing

--- pathfinder.c
+++ pathfinder.c
@@ -5,29 +5,24 @@ char    **pathfinder(data_val *data)
 {
     int i;
     int count;
-    char **current_positions;
+    char **todo;
     char ***paths;
 
     i = 0;
     count = 0;
     paths = (char **)malloc(10000);
+    
     // path gen then path select
-    current_possitions[0] = ft_strdup(data->start);
+
+    todo[0] = ft_strdup(data->start);
 
     // write algorithm to gen an array of all possible paths
     // write all paths to not include a duplicate string
-
-    
-
-    while(data->rooms)
+    while(todo[0])
     {
-        while(current_positions)
+        while(todo[count])
         {
-            while(ft_strstr(data->links, room_name(data->room[count]))
-            {
-                arr_add(current_positions, room_name(data->room([count])));
-                count++;
-            }
+            paths
         }
     }
 
